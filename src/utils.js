c785741b193a616d67fa9fbe0b25ced855ad35de
Korven <www.versionthreezero@gmail.com>
2023-08-06 12:02:13 +0300

    add progect

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
-const numberRandom = ((max = 100, min = 0) => Math.floor(Math.random() * (max - min + 1) + min));
+const numberRandom = ((max = 0, min = 100) => Math.floor(Math.random() * (max - min + 1) + min));
 
 export default numberRandom;
